04caaf9a019078e209f23f097c0c2a56c9a0ef12
Yazid <alouiyaz78@gmail.com>
2026-02-05 14:17:58 -0500

    Restore the netbook ingest_yellow_trip to the (4647f73) version

--- transformations/netbooks/00_landing/ingest_yellow_trips.py
+++ transformations/netbooks/00_landing/ingest_yellow_trips.py
@@ -1,39 +1,42 @@
 # Databricks notebook source
-import sys
+import urllib.request
 import os
+import shutil
+from datetime import datetime
+from datetime import date, datetime, timezone
+from dateutil.relativedelta import relativedelta
 
-project_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))
-if project_root not in sys.path:
-    sys.path.append(project_root)
+# Obtains the year-month for 2 months prior to the current month in yyyy-MM format
+two_months_ago = date.today() - relativedelta(months=1)
+formatted_date = two_months_ago.strftime("%Y-%m")
 
-# COMMAND ----------
-from modules.utils.date_utils import get_target_yyyy_mm
-from modules.data_loader.file_downloader import download_file
-
-# COMMAND ----------
-formatted_date = get_target_yyyy_mm(2)
-
+# Define the local directory for this date's data
 dir_path = f"/Volumes/nyctaxi/00_landing/data_sources/nyctaxi_yellow/{formatted_date}"
+
+# Define the full path for the downloaded file
 local_path = f"{dir_path}/yellow_tripdata_{formatted_date}.parquet"
 
-
-
-# COMMAND ----------
-# 1) If file exists, stop downstream
 try:
+    # Check if the file already exists
     dbutils.fs.ls(local_path)
-    
+
     # If the file already exists then set continue_downstream to no
     dbutils.jobs.taskValues.set(key="continue_downstream", value="no")
     print("File already downloaded, aborting downstream tasks")
-except :
+except:
     try:
         # Construct the URL for the Parquet file corresponding to this month
         url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{formatted_date}.parquet"
 
-        # Download the file
+        # Open a connection and stream the remote file
+        response = urllib.request.urlopen(url)
+
         # Create the local directory for this date's data
-        download_file(url, dir_path, local_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        # Save the streamed content to the local file in binary mode
+        with open(local_path, 'wb') as f:
+            shutil.copyfileobj(response, f)  # Copy data from response to file
         
         # Set continue_downstream to yes if the file was loaded
         dbutils.jobs.taskValues.set(key="continue_downstream", value="yes")
